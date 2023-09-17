7680c1092cb163004454db6f1cb982045ea7b6ec
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-09-17 14:43:32 -0600

    Divide and Conquer Sum implementation code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function divideAndConquerSum(a) {
-    return a;
+    // base case where array contains < 3 elements
+    if (a.length == 2)
+        return a[0] + a[1]
+    else if (a.length == 1)
+        return a[0]
+    else if (a.length == 0)
+        return 0;
+
+    // determines size of sub arrays
+    const slice = Math.round(a.length / 3)
+
+    // original array sliced into three sub arrays
+    const tmp1 = a.slice(0, slice)
+    const tmp2 = a.slice(slice, a.length - slice)
+    const tmp3 = a.slice(a.length - slice, a.length)
+
+    return divideAndConquerSum(tmp1) + divideAndConquerSum(tmp2) +
+        divideAndConquerSum(tmp3)
 }
